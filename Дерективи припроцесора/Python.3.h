d6c95ef45151eddf5b010c489c7c6cb269dc5b3f
AlexProstolucky <alexprostolucky@gmail.com>
2022-09-23 10:05:21 +0300

    Добавьте файлы проекта.

---
+++ Дерективи припроцесора/Python.3.h
@@ -0,0 +1,7 @@
+#pragma once
+#define more << " " << 
+#define print(шаблон) cout << шаблон << endl;
+#define input(шаблон) cin >> шаблон; 
+#define inpuT(шаблон, текст) cout << endl << текст; cin >> шаблон;
+#define and &&
+#define or ||
---
+++ Дерективи припроцесора/Дерективи припроцесора.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Python.3.h"
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	print("Hello World!" more a)
+		inpuT(a, "Input number --> ")
+		print(a)
+		if (a > 10 and a < 100)
+		{
+			print("A in diapasone(10,100)")
+		}
+		else if(a < 10 or a > 0) print("A in diapasone(0,10)")
+}
